5f6b5b0c854ecc065cbc914dcaeb49d412351a83
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-09-11 19:20:22 +0300

    Удаляет комментарии

--- js/main.js
+++ js/main.js
@@ -9,8 +9,6 @@ const refs = {
 
 const { galleryListEl, modalEl, modalImageEl, closeModalBtn } = refs;
 
-/* Создание и рендер разметки по массиву данных galleryItems из app.js и предоставленному шаблону.*/
-
 galleryListEl.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
 
 function createMarkup(data) {
@@ -29,8 +27,6 @@ function createMarkup(data) {
     .join('');
 }
 
-/* Реализация делегирования на галерее ul.js-gallery и получение url большого изображения. */
-
 galleryListEl.addEventListener('click', openFullImage);
 
 function openFullImage(event) {
@@ -43,19 +39,13 @@ function openFullImage(event) {
   }
 }
 
-/* Открытие модального окна по клику на элементе галереи. */
-
 function openModal() {
   modalEl.classList.add('is-open');
 }
 
-/* Подмена значения атрибута src элемента img.lightbox__image. */
-
 function createFullImage(event) {
   return (modalImageEl.src = event.target.dataset.source);
 }
-
-/* Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"] */
 
 closeModalBtn.addEventListener('click', closeModal);
 
@@ -63,5 +53,3 @@ function closeModal() {
   modalEl.classList.remove('is-open');
   modalImageEl.src = '';
 }
-
-/* Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее. */
