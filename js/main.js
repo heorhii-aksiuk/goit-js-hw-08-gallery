fa8e947f319b6ef81d900aa958db4115e65d457a
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-09-11 17:48:18 +0300

    Рефакторинг скрипта для разметки

--- js/main.js
+++ js/main.js
@@ -6,20 +6,20 @@ const refs = {
 
 const { galleryListEl } = refs;
 
-const markup = galleryItems.map(item => {
-  return ` <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="${item.original}"
-  >
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>
-</li>`;
-});
+galleryListEl.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
 
-galleryListEl.insertAdjacentHTML('afterbegin', markup.join(''));
+function createMarkup(data) {
+  return data
+    .map(item => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"/>
+        </a>
+      </li>`;
+    })
+    .join('');
+}
